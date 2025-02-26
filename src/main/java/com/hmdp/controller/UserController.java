428a9656ec21cd0ae0cc2038f4ddc6751c4d69b1
Joanna0001 <gkj159@163.com>
2025-02-26 16:36:01 +0800

    session ok

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.hmdp.controller;
 
-
-import cn.hutool.core.util.RandomUtil;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
-import com.hmdp.entity.User;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
-import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,20 +38,7 @@ public class UserController {
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
         // 发送短信验证码并保存验证码
-        // 校验手机号
-        if(RegexUtils.isPhoneInvalid(phone)) {
-            return Result.fail("手机号格式错误");
-        }
-
-        // 生成验证码
-        String code = RandomUtil.randomNumbers(6);
-
-        // 发送验证码
-        log.info("验证码：{}", code);
-
-        // 保存验证码到session
-        session.setAttribute("code", code);
-        return Result.ok(code);
+        return userService.sendCode(phone, session);
     }
 
     /**
@@ -63,25 +48,7 @@ public class UserController {
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
         // 实现登录功能
-        // 校验手机号
-        if(RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
-            return Result.fail("手机号格式错误");
-        }
-
-        // 校验验证码
-        String code = loginForm.getCode();
-        Object cacheCode = session.getAttribute("code");
-        if(cacheCode == null || !cacheCode.toString().equals(code)) {
-            return Result.fail("验证码错误");
-        }
-
-        // 根据手机号查询用户
-        String phone = loginForm.getPhone();
-        User user = query().eq("phone", phone).one();
-
-        // 判断用户是否存在，不存在则注册账号
-
-        return Result.ok("功能未完成");
+        return userService.login(loginForm, session);
     }
 
     /**
@@ -96,8 +63,9 @@ public class UserController {
 
     @GetMapping("/me")
     public Result me(){
-        // TODO 获取当前登录的用户并返回
-        return Result.fail("功能未完成");
+        // 获取当前登录的用户并返回
+        UserDTO user = UserHolder.getUser();
+        return Result.ok(user);
     }
 
     @GetMapping("/info/{id}")
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -14,7 +23,60 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 校验手机号
+        if(RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+
+        // 生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 发送验证码
+        log.info("验证码：{}", code);
+
+        // 保存验证码到session
+        session.setAttribute("code", code);
+        return Result.ok(code);
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 校验手机号
+        if(RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号格式错误");
+        }
+
+        // 校验验证码
+        String code = loginForm.getCode();
+        Object cacheCode = session.getAttribute("code");
+        if(cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误");
+        }
+
+        // 根据手机号查询用户
+        String phone = loginForm.getPhone();
+        User user = query().eq("phone", phone).one();
+
+        if(user == null) {
+            // 判断用户是否存在，不存在则注册账号
+            user = new User();
+            user.setPhone(phone);
+            user.setNickName("hm_" + RandomUtil.randomString(10));
+            user.setPassword("123456");
+
+            // 保存用户
+            save(user);
+        }
+
+        // 保存用户信息到session中
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+
+        return Result.ok();
+    }
 }
